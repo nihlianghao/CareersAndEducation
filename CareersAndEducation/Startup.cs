291cc28bebed409101cea9a0b9da1b6420da947a
nihlianghao <nih.leon@gmail.com>
2017-05-08 21:47:53 +1000

    Add project files.

---
+++ CareersAndEducation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CareersAndEducation.Startup))]
+namespace CareersAndEducation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
